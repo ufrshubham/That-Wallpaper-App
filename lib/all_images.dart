0ffbbcc646d56b240b56d2b99c46380a136da01a
Ryuzaki <ufrshubham@gmail.com>
2020-07-19 12:38:45 +0530

    GridView : All images

--- lib/all_images.dart
+++ lib/all_images.dart
@@ -1,17 +1,44 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class AllImages extends StatelessWidget {
-  const AllImages({Key key}) : super(key: key);
+class AllImages extends StatefulWidget {
+  AllImages({Key key}) : super(key: key);
 
   @override
+  _AllImagesState createState() => _AllImagesState();
+}
+
+class _AllImagesState extends State<AllImages> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'All Images!',
-          style: TextStyle(fontSize: 30.0),
-        ),
-      ),
+    return StreamBuilder(
+      stream: Firestore.instance.collection('wallpapers').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasData) {
+          return GridView.builder(
+            itemCount: snapshot.data.documents.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: const EdgeInsets.all(1.0),
+                child: GridTile(
+                  child: CachedNetworkImage(
+                    imageUrl: snapshot.data.documents.elementAt(index)['url'],
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              );
+            },
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+            ),
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 }

d505c0b8e39a7a2a51adb68aa1fd9b6240d1fee9
Ryuzaki <ufrshubham@gmail.com>
2020-07-25 15:41:32 +0530

    Wallpaper categories

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -11,15 +12,58 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final categories = List<String>();
+  final categoryImages = List<String>();
+
+  @override
+  void initState() {
+    super.initState();
+
+    widget.snapshot.data.documents.forEach((document) {
+      var category = document.data['tag'];
+
+      if (!categories.contains(category)) {
+        categories.add(category);
+        categoryImages.add(document.data['url']);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'Home!',
-          style: TextStyle(fontSize: 30.0),
-        ),
-      ),
+    return GridView.builder(
+      gridDelegate:
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1),
+      itemCount: categories.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          margin: EdgeInsets.all(5.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50.0),
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: CachedNetworkImageProvider(
+                categoryImages.elementAt(index),
+              ),
+            ),
+          ),
+          alignment: Alignment.center,
+          child: Text(
+            categories.elementAt(index).toUpperCase(),
+            style: TextStyle(
+              fontSize: 30.0,
+              color: Colors.white,
+              decoration: TextDecoration.combine(
+                [
+                  TextDecoration.underline,
+                  TextDecoration.overline,
+                ],
+              ),
+              decorationStyle: TextDecorationStyle.double,
+            ),
+          ),
+        );
+      },
     );
   }
 }

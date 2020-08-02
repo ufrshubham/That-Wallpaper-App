87cb2ad95f1ddb2eb6b8d6b2937eaaf199787a99
Ryuzaki <ufrshubham@gmail.com>
2020-08-02 19:35:53 +0530

    Category Wallpapers

---
+++ lib/category_wallpapers.dart
@@ -0,0 +1,49 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class CategoryWallpapers extends StatefulWidget {
+  final String category;
+
+  CategoryWallpapers({Key key, @required this.category}) : super(key: key);
+
+  @override
+  _CategoryWallpapersState createState() => _CategoryWallpapersState();
+}
+
+class _CategoryWallpapersState extends State<CategoryWallpapers> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('That Wallpaper App!'),
+      ),
+      body: StreamBuilder(
+        stream: Firestore.instance.collection('wallpapers').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasData) {
+
+            // Get all documents of widget.category.
+            var categoryDocuments = snapshot.data.documents
+                .where((document) => (document.data['tag'] == widget.category));
+
+            return ListView.builder(
+              itemCount: categoryDocuments.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: CachedNetworkImage(
+                    imageUrl: categoryDocuments.elementAt(index).data['url'],
+                  ),
+                );
+              },
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:that_wallpaper_app/category_wallpapers.dart';
 
 class Home extends StatefulWidget {
   final AsyncSnapshot<QuerySnapshot> snapshot;
@@ -36,30 +37,44 @@ class _HomeState extends State<Home> {
           SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1),
       itemCount: categories.length,
       itemBuilder: (BuildContext context, int index) {
-        return Container(
-          margin: EdgeInsets.all(5.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50.0),
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: CachedNetworkImageProvider(
-                categoryImages.elementAt(index),
+        return InkResponse(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return CategoryWallpapers(
+                    category: categories.elementAt(index),
+                  );
+                },
+              ),
+            );
+          },
+          child: Container(
+            margin: EdgeInsets.all(5.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50.0),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: CachedNetworkImageProvider(
+                  categoryImages.elementAt(index),
+                ),
               ),
             ),
-          ),
-          alignment: Alignment.center,
-          child: Text(
-            categories.elementAt(index).toUpperCase(),
-            style: TextStyle(
-              fontSize: 30.0,
-              color: Colors.white,
-              decoration: TextDecoration.combine(
-                [
-                  TextDecoration.underline,
-                  TextDecoration.overline,
-                ],
+            alignment: Alignment.center,
+            child: Text(
+              categories.elementAt(index).toUpperCase(),
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.white,
+                decoration: TextDecoration.combine(
+                  [
+                    TextDecoration.underline,
+                    TextDecoration.overline,
+                  ],
+                ),
+                decorationStyle: TextDecorationStyle.double,
               ),
-              decorationStyle: TextDecorationStyle.double,
             ),
           ),
         );
